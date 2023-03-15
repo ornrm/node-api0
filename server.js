0a1218a03883f0720c56620c352b94d4237b9294
ornrm <rujirangorn@gmail.com>
2023-03-15 16:49:43 +0700

    update

--- server.js
+++ server.js
@@ -31,26 +31,85 @@ app.get('/customers', function(req, res) {
   )
 })
 
+app.get('/orders', function(req, res) {
+  connection.query(
+    'SELECT * FROM a1_orders',
+    function(err, results) {
+      console.log(results) //แสดงผลที่ console
+      res.json(results) //ตอบกลับ request
+    }
+  )
+})
+
 app.get('/products', function(req, res) {
-    connection.query(
-      'SELECT * FROM a1_products',
-      function(err, results) {
-        console.log(results) //แสดงผลที่ console
-        res.json(results) //ตอบกลับ request
-      }
-    )
-  })
+  connection.query(
+    'SELECT * FROM a1_products',
+    function(err, results) {
+      console.log(results) //แสดงผลที่ console
+      res.json(results) //ตอบกลับ request
+    }
+  )
+})
 
-  app.get('/orders', function(req, res) {
-    connection.query(
-      'SELECT * FROM a1_orders',
-      function(err, results) {
-        console.log(results) //แสดงผลที่ console
-        res.json(results) //ตอบกลับ request
-      }
-    )
-  })
+// เรียงลับดับจากคนที่ซื้อเยอะ => น้อยที่สุด
+app.get("/top_customers", function (req, res) {
+  connection.query(
+    `SELECT 
+    C.firstname, 
+    SUM(O.QTY*P.Price) AS price_sum 
+  FROM a1_customers AS C 
+    INNER JOIN a1_Orders AS O ON C.Cid = O.Cid 
+    INNER JOIN a1_Products AS P ON O.Pid = P.Pid 
+  GROUP BY 
+    C.Cid
+  ORDER BY 
+    price_sum DESC;`,
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
 
+// เรียงลับดับจากคนที่ซื้อเยอะ => น้อยที่สุด
+app.get('/top_products', function(req, res){
+  connection.query(
+    `SELECT O.id, P.Pname, O.qtt, SUM(O.QTY*P.Price) as Total_QTY FROM a1_Orders as O INNER JOIN a1_products as P ON O.Pid = P.Pid GROUP BY O.id, P.name, O.qtt, P.price ORDER BY Total_QTY DESC;`,
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+
+app.post("/createusers", function (req, res) {
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const city = req.body.city;
+  const tel = req.body.tel;
+  connection.query(
+    `INSERT INTO a1_customers (firstname, lastname, email, city,  tel) VALUES (?, ?, ?, ?, ? ,? ,?)`,
+    [firstname, lastname, email, city, tel],
+    function (err, results) {
+      if (err) {
+        res.json(err);
+      }
+      res.json(results);
+    }
+  );
+});
+
+app.post('/orders', function(req, res) {
+  const values = req.body
+  console.log(values)
+  connection.query(
+    'INSERT INTO a1_orders (Oid, Cid, Pid, qtt) VALUES ?', [values],
+    function(err, results) {
+      console.log(results) //แสดงผลที่ console
+      res.json(results) //ตอบกลับ request
+    }
+  )
+})
 
 
 app.listen(5000, () => {
